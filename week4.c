8c3d89c144628f5112d3e9cf140ae0f9f9deeff6
krishnakoushik9 <legionkoushik3@gmail.com>
2025-08-12 10:43:47 +0530

    Week 4 minor change

--- week4.c
+++ week4.c
@@ -1,69 +1,40 @@
-#include<stdio.h>
-#include<string.h>
-#define INF 9999
+#include <stdio.h>
+#include <string.h>
+#include <time.h>
 
+#define Total_frames 10
+#define Window_size 4
+#define Loss_probability 20
+int is_frame_lost(){
+    return(rand()%100)<Loss_probability;
+}
 int main(){
-    int u,v,num,i,j,min,n;
-    int s[20],dist[20],path[20];
-    int cost[20][20];
-    printf("Enter no.of Vertices: ");
-    scanf("%d",&n);
-    printf("Enter the Adjacency matrix(-1 for no edge) \n");
-    for(i=0;i<n;i++){
-        for(j=0;j<n;j++){
-            scanf("%d",&cost[i][j]);
-            if(i==j){
-                cost[i][j]=0;
-            }else if(cost[i][j]==-1){
-                cost[i][j]=INF;
+    int base = 0,next_seq_num = 0;
+    int ack,i;
+    srand(time(NULL));
+    printf("Sliding Window Protocol with Go-Back-N Mechanism\n");
+    while(base < Total_frames){
+        printf("\n[Sender] Window:[%d to %d]\n", base, base + Window_size - 1);
+        for(i = base; i < base + Window_size && i < Total_frames; i++){
+            if(!is_frame_lost()){
+                printf("Sending frame %d\n", i);
+            } else {
+                printf("Frame %d lost\n", i);
+            }
+        }
+        if(i==base + Window_size || i == Total_frames){
+            base += Window_size;
+            if(base >= Total_frames) {
+                base = Total_frames; 
+                printf("[Receiver] ACK for frame %d.\n",base -1);
+            } else {
+                printf("[Receiver] No ACK received for frame %d. GO-BACK-N retransmission\n", base - 1);
+                printf("Retransmitting frames from %d to %d\n", base - Window_size, base - 1);
+                base -= Window_size; 
             }
         }
     }
-    printf("Enter source vertex(0 to %d): ",n-1);
-    scanf("%d",&v);
-    for(i=0;i<n;i++){
-        s[i]=0;
-        path[i]=v;
-        dist[i]=cost[v][i];
-    }    
-    dist[v]=0;
-    s[v]=1;
-    for(num = 1;num<n;num++){
-        min = INF;
-        u=-1;
-        for(i=0;i<n;i++){
-            if(!s[i] && dist[i] < min){
-                min = dist[i];
-                u=i;
-            }
-        }
-        if(u==-1){
-            break;
-        }
-        s[u] = 1;
-        for(i=0;i<n;i++){
-            if(!s[i] && dist[i] > dist[u] + cost[u][i]){
-                dist[i] = dist[u] + cost[u][i];
-                path[i] = u;
-            }
-        }
-    }
-    printf("\n Vertex \t Distance \t Path \n");
-    for(i=0;i<n;i++){
-        if(dist[i] == INF){
-            printf("%d\t INF\t\tNO Path\n",i);
-        }
-        else{
-            printf("%d\t%d\t\t",i,dist[i]);
-            j=i;
-            printf("%d",j);
-            while(path[j]!=v){
-                j = path[j];
-                printf("<- %d",j);
-            }
-            printf("<- %d",v);
-            printf("\n");
-        }
-    }
+    
+    printf("\nTransmission completed successfully!\n");
     return 0;
 }
