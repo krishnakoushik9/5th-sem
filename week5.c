5b11f7055bdb5a1eb0de8022e1be7b5e8e96cb71
krishnakoushik9 <legionkoushik3@gmail.com>
2025-08-19 10:10:41 +0530

    Week 5

---
+++ week5.c
@@ -0,0 +1,24 @@
+//Take an example SUBNET of hosts and obtain broadcast tree for the subnet
+#include <stdio.h>
+#define MAX 10
+
+int a[MAX][MAX], n;
+int visited[MAX];
+void broadcastTree(int root) {
+    int queue[MAX], front = 0, rear = 0;
+    for(int i = 0; i < n; i++) {
+        visited[i] = 0;
+    }
+    visited[root] = 1;
+    queue[rear++] = root;
+    printf("Broadcast Tree starting from node %d:\n", root);
+    while(front < rear) {
+        int current = queue[front++];
+        for(int j = 0; j < n; j++) {
+            if(a[current][j] == 1 && !visited[j]) {
+                visited[j] = 1;
+                queue[rear++] = j;
+                printf("Host %d -> %d\n",current + 1,j+1);
+            }
+        }
+}
