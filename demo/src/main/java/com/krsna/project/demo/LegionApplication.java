0cbcc8780ac6895852ce889bc032f469f21c7585
krishnakoushik9 <legionkoushik3@gmail.com>
2025-10-02 16:45:45 +0530

    Spring

---
+++ demo/src/main/java/com/krsna/project/demo/LegionApplication.java
@@ -0,0 +1,13 @@
+package com.krsna.project.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegionApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/krsna/project/demo/LegionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krsna.project.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
